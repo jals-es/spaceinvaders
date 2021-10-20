ebc4e484262c97755e5d14c2d8b8c28c643d135d
Antoni Tormo Garcia <gineraantoni@gmail.com>
2021-10-20 19:23:20 +0200

    Key detector

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,1 +1,24 @@
 /** SPACE INVADERS **/
+function getKeyAndMove(e) {
+    var key_code = e.which || e.keyCode;
+    switch (key_code) {
+        case 37: //left arrow key
+            // moveLeft();
+            break;
+        case 38: //Up arrow key
+            // moveUp();
+            break;
+        case 39: //right arrow key
+            // moveRight();
+            break;
+        case 40: //down arrow key
+            // moveDown();
+            break;
+        case 32: //Spacebar
+
+            break;
+        case 13: //Enter
+
+            break;
+    }
+}
