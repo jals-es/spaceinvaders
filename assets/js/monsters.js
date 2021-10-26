ef07c4a5afd39636ca3bc22d8fc36219888137ef
Juan Antonio López Seguí <juan.antonio.lis@gmail.com>
2021-10-26 15:32:11 +0200

    adding monsters

--- assets/js/monsters.js
+++ assets/js/monsters.js
@@ -4,71 +4,179 @@ image01_01.src = 'assets/img/01_01.svg';
 image01_01.width = 100;
 image01_01.height = 60;
 
-var m01_positions = [];
+var image02_01 = new Image(canvas);
+image02_01.src = 'assets/img/02_01.svg';
+image02_01.width = 100;
+image02_01.height = 75;
 
-function m01(x) {
-    this.image01 = image01_01;
+var image02_02 = new Image(canvas);
+image02_02.src = 'assets/img/02_02.svg';
+image02_02.width = 100;
+image02_02.height = 75;
+
+var image03_01 = new Image(canvas);
+image03_01.src = 'assets/img/03_01.svg';
+image03_01.width = 100;
+image03_01.height = 80;
+
+var image03_02 = new Image(canvas);
+image03_02.src = 'assets/img/03_02.svg';
+image03_02.width = 100;
+image03_02.height = 80;
+
+var m01_positions = [];
+var m02_positions = [];
+var m03_positions = [];
+var cant_monsters = 10;
+var margin_m = 20;
+
+function monster(x, y, image) {
+    this.image = image;
     this.x = x;
-    this.y = 0;
+    this.y = y;
 };
 
-var cant_monsters = 10;
+// function m02(x) {
+//     this.image02 = image02_01;
+//     this.x = x;
+//     this.y = image01_01.height + margin_m;
+// };
+
+// function m03(x) {
+//     this.image03 = image03_01;
+//     this.x = x;
+//     this.y = image01_01.height + image02_01.height + margin_m;
+// };
 
 var first_time_m = true;
 var seccond_time_m = false;
 const start_x_m = canvas.width / 2;
 
-// var m1_x = start_x + 1;
-// var m1_y = 0;
-// var m2_x = start_x + image01_01.width;
-// var m2_y = 0;
-// var m3_x = start_x + (image01_01.width * 2);
-// var m3_y = 0;
 var dx_m = 1;
+
+var count_seccond = 0;
 
 
 function print_monsters() {
     if (first_time_m) {
         first_time_m = false;
         print_monsters01_f();
+        print_monsters02_f();
+        print_monsters03_f();
     } else if (seccond_time_m) {
         check();
         print_monster01();
+        print_monster02();
+        print_monster03();
+        count_seccond++;
+    }
+
+    if (count_seccond == 100) {
+        count_seccond = 0;
+
+        for (let i = 0; i < m02_positions.length; i++) {
+
+            if (m02_positions[i].image.src === image02_01.src) {
+                m02_positions[i].image = image02_02;
+            } else {
+                m02_positions[i].image = image02_01;
+            }
+
+        }
+
+        for (let i = 0; i < m03_positions.length; i++) {
+
+            if (m03_positions[i].image.src === image03_01.src) {
+                m03_positions[i].image = image03_02;
+            } else {
+                m03_positions[i].image = image03_01;
+            }
+
+        }
+
+    } else if (count_seccond == 50) {
+        for (let i = 0; i < m03_positions.length; i++) {
+
+            if (m03_positions[i].image.src === image03_01.src) {
+                m03_positions[i].image = image03_02;
+            } else {
+                m03_positions[i].image = image03_01;
+            }
+
+        }
     }
 
 }
 
 function print_monsters01_f() {
-    let posicion = (canvas.width / 2) - ((cant_monsters * (image01_01.width + 20)) / 2);
+    let posicion = (canvas.width / 2) - ((cant_monsters * (image01_01.width + margin_m)) / 2);
+    let posicion_y = 0;
 
     let posicion_first = posicion;
-    // console.log(posicion);
 
     for (let i = 0; i < cant_monsters; i++) {
-        // console.log(posicion);
-        m01_positions[i] = new m01(posicion);
+        m01_positions[i] = new monster(posicion, posicion_y, image01_01);
 
-        ctx.drawImage(m01_positions[i].image01, m01_positions[i].x, m01_positions[i].y);
+        ctx.drawImage(m01_positions[i].image, m01_positions[i].x, m01_positions[i].y);
 
-        // console.log(posicion);
-        // console.log(posicion_first + (cant_monsters * m01.image01.width) - m01.image01.width);
-        if (posicion == (posicion_first + (cant_monsters * (image01_01.width + 20)) - (image01_01.width + 20))) {
+        if (posicion == (posicion_first + (cant_monsters * (image01_01.width + margin_m)) - (image01_01.width + margin_m))) {
             seccond_time_m = true;
-            console.log("entra")
         }
 
-        posicion = posicion + image01_01.width + 20;
+        posicion = posicion + image01_01.width + margin_m;
     }
-    console.log(m01_positions[3].x);
-    console.log(m01_positions);
+}
+
+function print_monsters02_f() {
+    let posicion = (canvas.width / 2) - ((cant_monsters * (image02_01.width + margin_m)) / 2);
+    let posicion_y = image01_01.height + margin_m;
+
+    for (let i = 0; i < cant_monsters; i++) {
+        m02_positions[i] = new monster(posicion, posicion_y, image02_01);
+
+        ctx.drawImage(m02_positions[i].image, m02_positions[i].x, m02_positions[i].y);
+
+        posicion = posicion + image02_01.width + margin_m;
+    }
+}
+
+function print_monsters03_f() {
+    let posicion = (canvas.width / 2) - ((cant_monsters * (image03_01.width + margin_m)) / 2);
+    let posicion_y = image01_01.height + margin_m + image02_01.height + margin_m;
+
+    for (let i = 0; i < cant_monsters; i++) {
+        m03_positions[i] = new monster(posicion, posicion_y, image03_01);
+
+        ctx.drawImage(m03_positions[i].image, m03_positions[i].x, m03_positions[i].y);
+
+        posicion = posicion + image03_01.width + margin_m;
+    }
 }
 
 function print_monster01() {
     for (let i = 0; i < m01_positions.length; i++) {
 
-        ctx.drawImage(m01_positions[i].image01, m01_positions[i].x, m01_positions[i].y);
+        ctx.drawImage(m01_positions[i].image, m01_positions[i].x, m01_positions[i].y);
 
         m01_positions[i].x += dx_m;
+    }
+}
+
+function print_monster02() {
+    for (let i = 0; i < m02_positions.length; i++) {
+
+        ctx.drawImage(m02_positions[i].image, m02_positions[i].x, m02_positions[i].y);
+
+        m02_positions[i].x += dx_m;
+    }
+}
+
+function print_monster03() {
+    for (let i = 0; i < m03_positions.length; i++) {
+
+        ctx.drawImage(m03_positions[i].image, m03_positions[i].x, m03_positions[i].y);
+
+        m03_positions[i].x += dx_m;
     }
 }
 
@@ -79,8 +187,9 @@ function check() {
     } else if (m01_positions[0].x == 0) {
         dx_m = 1;
         baja(50);
-    } else if (m01_positions[m01_positions.length - 1].y >= n_y) {
+    } else if (m03_positions[m03_positions.length - 1].y >= n_y) {
         alert("DERROTA")
+        baja(-m01_positions[m01_positions.length - 1].y)
     }
 }
 
@@ -88,5 +197,15 @@ function baja(cant) {
     for (let i = 0; i < m01_positions.length; i++) {
 
         m01_positions[i].y += cant;
+    }
+
+    for (let i = 0; i < m02_positions.length; i++) {
+
+        m02_positions[i].y += cant;
+    }
+
+    for (let i = 0; i < m03_positions.length; i++) {
+
+        m03_positions[i].y += cant;
     }
 }
