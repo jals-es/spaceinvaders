6dd4cb853eaafad0a02427ce47732041a587747e
Juan Antonio López Seguí <juan.antonio.lis@gmail.com>
2021-11-04 20:48:26 +0100

    message move x

--- assets/js/screens.js
+++ assets/js/screens.js
@@ -4,6 +4,8 @@ var lose_font = 50;
 var final_score_font = 0;
 var press_start_font = 50;
 var move_press_start_font = 1;
+var x_press_start_font = (canvas.width / 2) - 400;
+var move_x_press_start_font = 1;
 var count_lose = 0;
 var color_lose = colorHEX();
 
@@ -20,6 +22,7 @@ function print_lose() {
         press_start_font += move_press_start_font;
     }
 
+    x_press_start_font += move_press_start_font * -1;
 
 
     ctx.fillStyle = "red";
@@ -51,7 +54,7 @@ function print_lose() {
 
     ctx.font = press_start_font + "px DotsAllForNow";
 
-    ctx.fillText("Press enter to restart", (canvas.width / 2) - 450, (canvas.height / 2) + 250);
+    ctx.fillText("Press enter to restart", x_press_start_font, (canvas.height / 2) + 250);
 
 }
 
@@ -66,6 +69,8 @@ function print_win() {
     if (count_lose == 5 || count_lose == 10 || count_lose == 15 || count_lose == 20 || count_lose == 25) {
         press_start_font += move_press_start_font;
     }
+
+    x_press_start_font += move_press_start_font * -1;
 
 
 
@@ -97,7 +102,7 @@ function print_win() {
 
     ctx.font = press_start_font + "px DotsAllForNow";
 
-    ctx.fillText("Press enter to restart", (canvas.width / 2) - 450, (canvas.height / 2) + 250);
+    ctx.fillText("Press enter to restart", x_press_start_font, (canvas.height / 2) + 250);
 }
 
 function restart_game() {
